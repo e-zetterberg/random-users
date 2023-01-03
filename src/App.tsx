3b5d79444e1590b6ed5f3592aad4dd359f250e54
Erik Zetterberg <erik.zetterberg@appliedtechnology.se>
2023-01-03 14:06:09 +0100

    working prototype

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import './App.css';
+import React from "react";
+import { useEffect, useState } from "react";
+import "./App.css";
 
 interface UserData {
   name: {
     title: string;
     first: string;
     last: string;
-  }
+  };
   dob: {
-    age: string
-  }
+    age: string;
+  };
   location: {
     city: string;
     street: {
@@ -19,40 +19,90 @@ interface UserData {
     };
     country: string;
     postcode: number;
+  };
+  picture:{
+    large: string;
+    medium: string;
+    thumbnail: string;
   }
-};
+}
 
 function App() {
   const [userData, setUserData] = useState<UserData | null>(null);
-
+  const [isEditing, setIsEditing] = useState(false);
+  const [firstName, setFirstName] = useState(userData?.name.first);
   useEffect(() => {
     fetchUserData().then((data) => {
       setUserData(data);
     });
   }, []);
 
+  useEffect(() => {
+    setFirstName(userData?.name.first);
+  },[userData]);
+
   const fetchUserData = async () => {
-    const response = await fetch('https://randomuser.me/api/');
+    const response = await fetch("https://randomuser.me/api/");
     const data = await response.json();
     console.log(data.results[0]);
     return data.results[0];
   };
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsEditing(false);
+  }
+
+  const updateUser = () => {
+    fetchUserData().then((data) => {
+      setUserData(data);
+    });
+    setIsEditing(false);
+  };
+
+
   return (
-    <main className="App">
-      <header>
-        Random User
-      </header>
-      <div>
-        <span>Name: {userData?.name.first}</span>
-      </div>
-      <div>
-        <span>Age: {userData?.dob.age}</span>
-      </div>
-      <div>
-        <span>Address :{userData?.location.city}</span>
-      </div>
-    </main>
+    <>
+      <header className="App-header">Random User</header>
+      <main className="App">
+        <section className="App--image-container">
+          <img src={userData?.picture.large} alt="" />
+        </section>
+        <section className="App--info-container">
+        <form 
+        onSubmit={(e) => handleSubmit(e)} 
+        >
+          {'Name: '}
+          <input 
+          type="text" 
+          onClick={() => setIsEditing(true)}
+          onChange={(e) => setFirstName(e.target.value)}
+          readOnly={!isEditing}
+          value={firstName}
+          />
+          <button 
+          type="submit"
+          hidden={!isEditing}
+          >
+            Save
+          </button>
+        </form>
+        <div>
+          Age: {userData?.dob.age}
+        </div>
+        <div>
+          <div>Address: </div>
+          <span> {userData?.location.street.number} </span>
+          <span>{userData?.location.street.name}</span>
+          <div>{userData?.location.postcode}</div>
+          <div>{userData?.location.city}</div>
+          <div>{userData?.location.country}</div>
+        </div>
+        </section>
+         <button className="btn--new-user" onClick={() => updateUser()}>New User</button>
+
+      </main>
+    </>
   );
 }
 
